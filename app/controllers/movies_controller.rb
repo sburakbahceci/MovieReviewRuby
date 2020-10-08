12fed5db754d133c22fa8c021394c355f227c972
sburakbahceci <safaburakbahceci29@gmail.com>
2020-10-08 15:01:07 +0300

    Add paperclip for image upload

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -70,6 +70,6 @@ class MoviesController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def movie_params
-      params.require(:movie).permit(:title, :description, :movie_length, :director, :rating)
+      params.require(:movie).permit(:title, :description, :movie_length, :director, :rating, :image)
     end
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,5 @@
 class Movie < ApplicationRecord
     belongs_to :user
+    has_attached_file :image, styles: { medium: "400x600#" }
+    validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
